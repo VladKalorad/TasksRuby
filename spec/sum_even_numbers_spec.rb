ffea40f8f718619b628e43ecb51e015e1263c23b
VladKalorad <vladchernav210@gmail.com>
2022-10-26 12:47:09 +0300

    fix test

--- spec/sum_even_numbers_spec.rb
+++ spec/sum_even_numbers_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe do
   subject(:sum) { SumEvenNumbers.new(seq) }
 
   describe '#call' do
-    context 'when string include fullname' do
+    context 'when calculates the sum of even numbers' do
       let(:seq) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
 
       it { expect(sum.call).to eq(30) }
