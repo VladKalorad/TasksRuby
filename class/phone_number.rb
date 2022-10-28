6dcc88d8dc54798036a98c158433ea73ea96e587
VladKalorad <vladchernav210@gmail.com>
2022-10-28 22:57:45 +0300

    add phone_number and test

--- class/phone_number.rb
+++ class/phone_number.rb
@@ -3,10 +3,12 @@
 class PhoneNumber
   attr_accessor :numbers
 
-  def create_phone_number
-    puts "(#{@numbers[0]}#{@numbers[1]}#{@numbers[2]}) #{@numbers[3]}#{@numbers[4]}#{@numbers[5]}-#{@numbers[6]}#{@numbers[7]}#{@numbers[8]}#{@numbers[9]}"
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  def call
+    "(#{@numbers[0]}#{@numbers[1]}#{@numbers[2]}) #{@numbers[3]}#{@numbers[4]}#{@numbers[5]}-#{@numbers[6]}#{@numbers[7]}#{@numbers[8]}#{@numbers[9]}"
   end
 end
-task5 = PhoneNumber.new
-task5.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-task5.create_phone_number
+puts PhoneNumber.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]).call
