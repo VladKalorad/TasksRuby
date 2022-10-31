444eb786f368abc37714802f336cde72df38890d
VladKalorad <vladchernav210@gmail.com>
2022-10-31 22:29:42 +0300

    add Alarn and test

---
+++ spec/alarm_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../class/alarm'
+require_relative 'spec_helper'
+
+RSpec.describe do
+  describe('#call') do
+    context 'when employed ans vacation equal true' do
+      let(:employed) { true }
+      let(:vacation) { true }
+      it { expect(SetAlarm.new(employed, vacation).call).to eq(false) }
+    end
+  end
+end
