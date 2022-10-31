779ee656da36ace556fed8977547e4882f8ba22c
VladKalorad <vladchernav210@gmail.com>
2022-10-31 21:59:06 +0300

    ExesAndOhs fix and test

---
+++ spec/exes_and_ohs_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../class/exes_and_ohs'
+require_relative 'spec_helper'
+
+RSpec.describe do
+  describe('#call') do
+    context 'when the offer is X and O' do
+      let(:string) { 'xooxxoo' }
+      it { expect(ExesAndOhs.new(string).call).to eq(false) }
+    end
+  end
+end
